8059bcb38acaf28692ed1e9b01d28c274b112f79
userSandeepB <57865036+userSandeepB@users.noreply.github.com>
2019-12-15 20:01:01 +0530

    function expression and function statements

--- 01-basics/functions/functions.js
+++ 01-basics/functions/functions.js
@@ -1,3 +1,4 @@
+// Function Statement or Declaration
 function calculateAge(birthYear) {
   return 2019 - birthYear;
 }
@@ -15,3 +16,21 @@ function yearsUntilRetirement(birthYear, firstName) {
 yearsUntilRetirement(1990, "John");
 yearsUntilRetirement(1948, "Mike");
 yearsUntilRetirement(1969, "Jane");
+
+// Function Expression
+var whatDoYouDo = function(job, firstName) {
+  switch (job) {
+    case "teacher":
+      return firstName + " teaches kid how to code.";
+    case "driver":
+      return firstName + " drives a cab in Delhi";
+    case "designer":
+      return firstName + " design beautiful website";
+    default:
+      return firstName + " does something else";
+  }
+};
+
+console.log(whatDoYouDo("teacher", "John"));
+console.log(whatDoYouDo("designer", "Jane"));
+console.log(whatDoYouDo("retired", "Mike"));
