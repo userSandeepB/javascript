8f349ebda6e1dab12b61291396dc2f4dcd9bcdf2
userSandeepB <57865036+userSandeepB@users.noreply.github.com>
2019-12-22 10:07:45 +0530

    revisited functions and basics

--- 01-basics/basics/script.js
+++ 01-basics/basics/script.js
@@ -1,10 +1,10 @@
 // Variables and Data Types
 var firstName = "Peter";
-var age = 7;
+var birthYear = 2013;
 var isAdult = false;
 var job;
 var favMovie = null;
 
 // Type Coercion
-var result = firstName + age;
+var result = firstName + birthYear;
 console.log(`${result} is ${typeof result}`);
--- 01-basics/functions/script.js
+++ 01-basics/functions/script.js
@@ -1,36 +1,12 @@
-// Function Statement or Declaration
-function calculateAge(birthYear) {
+// 01. Function Declaration
+function calcAge(birthYear) {
   return 2019 - birthYear;
 }
 
-function yearsUntilRetirement(birthYear, firstName) {
-  var age = calculateAge(birthYear);
-  var retirement = 65 - age;
-  if (retirement > 0) {
-    console.log(firstName + " retires in " + (65 - age) + " years.");
-  } else {
-    console.log(firstName + " is already retired.");
-  }
-}
-
-yearsUntilRetirement(1990, "John");
-yearsUntilRetirement(1948, "Mike");
-yearsUntilRetirement(1969, "Jane");
-
-// Function Expression
-var whatDoYouDo = function(job, firstName) {
-  switch (job) {
-    case "teacher":
-      return firstName + " teaches kid how to code.";
-    case "driver":
-      return firstName + " drives a cab in Delhi";
-    case "designer":
-      return firstName + " design beautiful website";
-    default:
-      return firstName + " does something else";
-  }
+// 02. Function Expression
+var isAdult = function(birthYear) {
+  var age = calcAge(birthYear);
+  return age >= 18 ? true : false;
 };
 
-console.log(whatDoYouDo("teacher", "John"));
-console.log(whatDoYouDo("designer", "Jane"));
-console.log(whatDoYouDo("retired", "Mike"));
+console.log(isAdult(2013));
